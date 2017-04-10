86a936268371731b56c354df17ef7673bfba2977
aliona Kuharenko <alionakuharenko@gmail.com>
2017-04-10 21:04:00 +0000

    Change8

--- js/index.js
+++ js/index.js
@@ -2,4 +2,14 @@ $('.button').click(function() {
     location.href = "form.html";
 });
 
- 
+ $(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1500);
+        }
+    });
+});
